75790738ce29d0b2310d7fa10de1e154e84d1ad5
ingrid-mo <morales.ingrid.i20@gmail.com>
2024-02-07 18:41:29 -0300

    cambie estructura de footercomponet

--- src/components/FooterComponet/Footer.jsx
+++ src/components/FooterComponet/Footer.jsx
@@ -1,23 +1,17 @@
-// import React from "react"; 
+// import React from "react";
 import "./Styles.css";
-import imagenes from '../../../../../public/assets/img'
+import imagenes from "../../../public/assets/img";
 import Image from "next/image";
-
-
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="footer__title">
         <div className="footer__body">
-          <Image
-          src={imagenes.logo}
-          />
-        
+          <Image src={imagenes.logo} />
+
           <div className="footer__body__contact">
-          <Image
-          src={imagenes.map}
-          />
+            <Image src={imagenes.map} />
             <div>
               <p>
                 Direccion <br /> Bustamante 26, Providencia.
@@ -26,9 +20,7 @@ const Footer = () => {
           </div>
 
           <div className="footer__body__contact">
-          <Image
-          src={imagenes.enviar}
-          />
+            <Image src={imagenes.enviar} />
             <div>
               <p>
                 Email <br /> hola@rondachile.cl
@@ -37,9 +29,7 @@ const Footer = () => {
           </div>
 
           <div className="footer__body__contact">
-          <Image
-          src={imagenes.phone}
-          />
+            <Image src={imagenes.phone} />
             <div>
               <p>
                 Teléfono <br /> +56 9 3261 7984
@@ -48,25 +38,32 @@ const Footer = () => {
           </div>
         </div>
         <div className="footer__logo">
-        <Image
-          src={imagenes.facebook}
-          /><Image
-          src={imagenes.instagram}
-          /><Image
-          src={imagenes.X}
-          /><Image
-          src={imagenes.youtube}
-          />
+          <a href="">
+            <Image src={imagenes.facebook} />
+          </a>
+          <a href="">
+            <Image src={imagenes.instagram} />
+          </a>
+          <a href="">
+            <Image src={imagenes.X} />
+          </a>
+          <a href="">
+            <Image src={imagenes.youtube} />
+          </a>
         </div>
         <hr className="footer__line" />
         <div className="footer__footer">
-          <div> <p> Sitio creado por el equipo de desarrollo</p></div>
+          <div>
+            <p> Sitio creado por el equipo de desarrollo</p>
+          </div>
+          <a href="">
          
-          <Image
-          src={imagenes.dllogo}
-          />
-          <div><p>2024</p></div>
-          
+            <Image src={imagenes.dllogo} />
+          </a>
+
+          <div>
+            <p>2024</p>
+          </div>
         </div>
       </div>
     </div>
