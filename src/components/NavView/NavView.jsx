305c50351803902412a2c4b81f1e5e336bc5cfa0
Diego-Fdez <dieghoxcr7@gmail.com>
2024-02-07 14:28:50 -0600

    create component nav

--- src/components/NavView/NavView.jsx
+++ src/components/NavView/NavView.jsx
@@ -1,10 +1,29 @@
+'use client';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import styles from './styles/NavView.module.css';
+import { ROUTES_INFO } from './constants';
+
 const NavView = () => {
+  const pathname = usePathname();
+
   return (
-    <nav>
-      <ul>
-        <li>Home</li>
-      </ul>
-    </nav>
+    <header className={styles.navContainer}>
+      <nav className={styles.nav}>
+        {ROUTES_INFO.map((route) => (
+          <Link
+            className={`${styles.link} ${
+              pathname === route.pathname && styles.active
+            }`}
+            href={route.pathname}
+            key={route.id}
+          >
+            {pathname === route.pathname && route.icon}
+            {route.title}
+          </Link>
+        ))}
+      </nav>
+    </header>
   );
 };
 
