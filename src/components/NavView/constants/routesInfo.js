ab1d78eff70291f899ea7f474786b93e093fe663
Diego-Fdez <dieghoxcr7@gmail.com>
2024-02-07 14:26:48 -0600

    create array with routes information

---
+++ src/components/NavView/constants/routesInfo.js
@@ -0,0 +1,44 @@
+import { HomeIcon } from '../icons';
+
+/**
+ * This code defines an array called ROUTES_INFO that contains information about different paths in an application.
+ * Each object in the array represents a path and has properties such as id, pathname, title, and icon.
+ */
+export const ROUTES_INFO = [
+  {
+    id: 1,
+    pathname: '/',
+    title: 'Inicio',
+    icon: <HomeIcon />,
+  },
+  {
+    id: 2,
+    pathname: '/about',
+    title: 'Quienes Somos',
+    icon: <HomeIcon />,
+  },
+  {
+    id: 3,
+    pathname: '/program',
+    title: 'Programa',
+    icon: <HomeIcon />,
+  },
+  {
+    id: 4,
+    pathname: '/academy',
+    title: 'Academia',
+    icon: <HomeIcon />,
+  },
+  {
+    id: 5,
+    pathname: '/resources',
+    title: 'Recursos de apoyo',
+    icon: <HomeIcon />,
+  },
+  {
+    id: 6,
+    pathname: '/forum',
+    title: 'Foro',
+    icon: <HomeIcon />,
+  },
+];
