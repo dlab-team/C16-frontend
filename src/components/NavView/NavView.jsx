8cd757aed817b3b40d24c862aa7564b7d4d62f0d
Diego-Fdez <dieghoxcr7@gmail.com>
2024-02-08 14:36:42 -0600

    add buttons

--- src/components/NavView/NavView.jsx
+++ src/components/NavView/NavView.jsx
@@ -1,22 +1,32 @@
 'use client';
-import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './styles/NavView.module.css';
 import { ROUTES_INFO } from './constants';
+import { MenuIcon } from './icons';
+import { useNavView } from './hook';
 
 const NavView = () => {
-  const pathname = usePathname();
+  const { pathname, toggleMenu } = useNavView();
 
   return (
     <header className={styles.navContainer}>
-      <nav className={styles.nav}>
+      <Image
+        src="/assets/logo-ronda.svg"
+        alt="logo Ronda"
+        width={53}
+        height={53}
+        loading="lazy"
+        className={styles.logo}
+      />
+      <nav id="navContainer" className={styles.nav}>
         {ROUTES_INFO.map((route) => (
           <Link
             className={`${styles.link} ${
               pathname === route.pathname && styles.active
             }`}
             href={route.pathname}
+            onClick={toggleMenu}
             key={route.id}
           >
             {/* ToDo: Do all items have an icon or none? */}
@@ -33,6 +43,12 @@ const NavView = () => {
           className={styles.profileImage}
         />
       </nav>
+      <div className={styles.buttonsContainer}>
+        <button className={styles.loginButton}>Ingresar</button>
+        <button onClick={toggleMenu} className={styles.menuButton}>
+          <MenuIcon />
+        </button>
+      </div>
     </header>
   );
 };
