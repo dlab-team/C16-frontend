bbfd560a8330b14c59f635f13d65d200545c90da
Diego-Fdez <dieghoxcr7@gmail.com>
2024-02-08 17:22:16 -0600

    fix nav styles

--- src/components/NavView/hook/useNavView.jsx
+++ src/components/NavView/hook/useNavView.jsx
@@ -1,29 +1,48 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { usePathname } from 'next/navigation';
 
 const useNavView = () => {
   const pathname = usePathname();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  //The "changeNavStyle" function changes the display style of a navigation container based on the "isMenuOpen" variable.
-  function changeNavStyle() {
-    const navContainer = document.getElementById('navContainer');
+  useEffect(() => {
+    //function to change the nav style based on the isMenuOpen state
+    const changeNavStyle = () => {
+      const navContainer = document.getElementById('navContainer');
+      if (isMenuOpen) {
+        navContainer.style.display = 'flex';
+      } else {
+        navContainer.style.display = 'none';
+      }
+    };
 
-    if (isMenuOpen) {
-      navContainer.style.display = 'flex';
-    } else {
-      navContainer.style.display = 'none';
-    }
-  }
-
-  /**
-   * Function to toggle the menu.
-   * Change the state of isMenuOpen and call the changeNavStyle function.
-   */
-  function toggleMenu() {
-    setIsMenuOpen(!isMenuOpen);
     changeNavStyle();
-  }
+  }, [isMenuOpen]);
+
+  useEffect(() => {
+    /*function to reset the nav styles to default when the window is resized to a width greater than 768px. 
+    This is to avoid the nav styles from being applied to the mobile menu when the window is resized to a width less than 768px. */
+    const resetDesktopStyles = () => {
+      const navContainer = document.getElementById('navContainer');
+      navContainer.style.display = 'flex';
+    };
+
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        resetDesktopStyles();
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prevState) => !prevState);
+  };
 
   return {
     pathname,
