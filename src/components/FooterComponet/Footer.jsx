8aaa243831b2f06926a5e47908970d1b7b6c817d
ingrid-mo <morales.ingrid.i20@gmail.com>
2024-02-07 22:19:47 -0300

    modifique la imagen svg del fondo, cambie la forma de posicionarla, la adicione al jsx y la saque del css, ademas modifique la secion de contacto

--- src/components/FooterComponet/Footer.jsx
+++ src/components/FooterComponet/Footer.jsx
@@ -6,34 +6,46 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <div className="footer">
+      <div className="footer__background">
+        <svg
+          width="1457"
+          height="373"
+          viewBox="0 0 1457 373"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M86.8837 17.0326C15.9053 38.1829 0 59.1176 0 59.1176L8.55339 373H1457L1457 77.3184C1457 77.3184 1208.14 128.05 1050.96 113.258C824.076 91.9051 757.694 87.3989 583.186 48.0193C448.327 17.5871 223.661 -23.7245 86.8837 17.0326Z"
+            fill="#FFC5E4"
+            c
+          />
+        </svg>
+      </div>
+
       <div className="footer__title">
         <div className="footer__body">
           <Image src={imagenes.logo} />
 
           <div className="footer__body__contact">
             <Image src={imagenes.map} />
-            <div>
-              <p>
-                Direccion <br /> Bustamante 26, Providencia.
-              </p>
+
+            <div className="footer__text">
+              <h5> Direccion</h5> <p>Bustamante 26, Providencia.</p>
             </div>
           </div>
 
           <div className="footer__body__contact">
             <Image src={imagenes.enviar} />
-            <div>
-              <p>
-                Email <br /> hola@rondachile.cl
-              </p>
+            <div className="footer__text">
+              <h5>Email</h5> <p>hola@rondachile.cl</p>
             </div>
           </div>
 
           <div className="footer__body__contact">
             <Image src={imagenes.phone} />
-            <div>
-              <p>
-                Teléfono <br /> +56 9 3261 7984
-              </p>
+
+            <div className="footer__text">
+              <h5> Teléfono</h5> <p> +56 9 3261 7984</p>
             </div>
           </div>
         </div>
@@ -57,7 +69,6 @@ const Footer = () => {
             <p> Sitio creado por el equipo de desarrollo</p>
           </div>
           <a href="">
-         
             <Image src={imagenes.dllogo} />
           </a>
 
