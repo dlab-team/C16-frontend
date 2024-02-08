40ed451c6da6090a1b319ce912e4a4928b76ae35
Diego-Fdez <dieghoxcr7@gmail.com>
2024-02-08 14:26:17 -0600

    create SVG menuIcon

---
+++ src/components/NavView/icons/MenuIcon.jsx
@@ -0,0 +1,33 @@
+const MenuIcon = () => (
+  <svg
+    width="43"
+    height="38"
+    viewBox="0 0 43 38"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M5.375 19H37.625"
+      stroke="black"
+      strokeWidth="3"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M5.375 9.5H37.625"
+      stroke="black"
+      strokeWidth="3"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M5.375 28.5H37.625"
+      stroke="black"
+      strokeWidth="3"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default MenuIcon;
