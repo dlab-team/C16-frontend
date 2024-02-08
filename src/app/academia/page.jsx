4cdb30f25a137cb7bcf2bbd66b7207eea0f07ae0
manuel14mds <manuel14mds@gmail.com>
2024-02-08 08:48:17 -0500

    Add card redirection link

---
+++ src/app/academia/page.jsx
@@ -0,0 +1,10 @@
+
+function Academia() {
+    return (
+        <div>
+            <h1 style={{textAlign: "center", padding:"20px"}}>Academia page</h1>
+        </div>
+    )
+}
+
+export default Academia
