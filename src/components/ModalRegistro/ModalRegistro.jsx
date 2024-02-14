45c9cdce2fcd13385429e898c56617f45fc83b43
Sebastian <sebas@Sebastians-MacBook-Air.local>
2024-02-14 16:23:08 -0500

    Add modal registro

---
+++ src/components/ModalRegistro/ModalRegistro.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './Modal.module.css';
+
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className={styles.modalOverlay}>
+      <div className={styles.modal}>
+        <button className={styles.closeButton} onClick={onClose}>
+           Cerrar
+        </button>
+        <div className={styles.modalContent}>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
+
+  
