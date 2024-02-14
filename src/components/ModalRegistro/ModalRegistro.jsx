4062c90e533fbdfb7c50b1870706be575a9882f5
Sebastian <sebas@Sebastians-MacBook-Air.local>
2024-02-14 17:00:21 -0500

    Format code

--- src/components/ModalRegistro/ModalRegistro.jsx
+++ src/components/ModalRegistro/ModalRegistro.jsx
@@ -2,20 +2,18 @@ import React from 'react';
 import styles from './Modal.module.css';
 
 const Modal = ({ isOpen, onClose, children }) => {
-  if (!isOpen) return null;
+    if (!isOpen) return null;
 
-  return (
-    <div className={styles.modalOverlay}>
-      <div className={styles.modal}>
-        <button className={styles.closeButton} onClick={onClose}>
-           Cerrar
-        </button>
-        <div className={styles.modalContent}>{children}</div>
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.modalOverlay}>
+            <div className={styles.modal}>
+                <button className={styles.closeButton} onClick={onClose}>
+                    X
+                </button>
+                <div className={styles.modalContent}>{children}</div>
+            </div>
+        </div>
+    );
 };
 
 export default Modal;
-
-  
