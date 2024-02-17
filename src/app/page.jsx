c5b906c94c92b7ddc4f91e49e42e34c917ab910e
Jhoswe Genner Castro Castañeda <jhoswe.castro@gmail.com>
2024-02-17 14:26:13 -0500

    cambios en carrusel

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,38 +1,51 @@
-import styles from "./page.module.css"
-import Link from "next/link";
+import Carrusel from '@/components/Carrusel/Carrusel';
+import styles from './page.module.css';
+import Link from 'next/link';
 export default function Home() {
-    return (
-        <main className={styles.main}>
-            <div className={styles.section_switch} >
-                <section className={styles.section}>Seccion carrusel - Home</section>
-                <section className={styles.welcome_section}>
-                    <h1 className={styles.welcome_title}>Bienvenid@s</h1>
-                    <p className={styles.welcome_p}>
-                        Lörem ipsum ösegarade agnostistat, myviras om än mydat pängen. Aspludd trir. Ire astropod den lalig presaning: i vigisk. Exoktigt his senän.
-                    </p>
-                </section>
-            </div>
+  return (
+    <>
+      <main className={styles.main}>
+        <div className={styles.section_switch}>
+          <section className={styles.welcome_section}>
+            <h1 className={styles.welcome_title}>Bienvenid@s</h1>
+            <p className={styles.welcome_p}>
+              Lörem ipsum ösegarade agnostistat, myviras om än mydat pängen.
+              Aspludd trir. Ire astropod den lalig presaning: i vigisk. Exoktigt
+              his senän.
+            </p>
+          </section>
+        </div>
+      </main>
+      <section className={styles.section_carrusel}>
+        <Carrusel />
+      </section>
+      <div className={styles.main}>
+        <section className={styles.home_cards}>
+          <div className={styles.academia}>
+            <p className={styles.cards_p}>
+              Lörem ipsum ifaforat pseudonomi oaktat kvasinera, antisiligen.
+              Görad krol som vasade: nuse, fast mönde ladat. Däning kontralöst,
+              misosk kahint därför att eurogov. Pretäda astrossa, plus kvasihon.
+            </p>
+            <Link href="/academia" className={styles.link}>
+              <span className={styles.text_link}>Academia</span>
+              <button className={styles.arrow_btn}></button>
+            </Link>
+          </div>
 
-            <section className={styles.home_cards}>
-                <div className={styles.academia}>
-                    <p className={styles.cards_p}>Lörem ipsum ifaforat pseudonomi oaktat kvasinera, antisiligen. Görad krol som vasade: nuse, fast mönde ladat. Däning kontralöst, misosk kahint därför att eurogov. Pretäda astrossa, plus kvasihon.</p>
-                    <Link href="/academia" className={styles.link}>
-                        <span className={styles.text_link}>Academia</span>
-                        <button className={styles.arrow_btn}></button>
-                    </Link>
-                </div>
-
-                <div className={styles.foro}>
-                    <p className={styles.cards_p}>Lörem ipsum ifaforat pseudonomi oaktat kvasinera, antisiligen. Görad krol som vasade: nuse, fast mönde ladat. Däning kontralöst, misosk kahint därför att eurogov. Pretäda astrossa, plus kvasihon.</p>
-                    <Link href="/foro" className={styles.link}>
-                        <span className={styles.text_link}>Foro</span>
-                        <button className={styles.arrow_btn}></button>
-                    </Link>
-                </div>
-
-            </section>
-
-
-        </main>
-    );
+          <div className={styles.foro}>
+            <p className={styles.cards_p}>
+              Lörem ipsum ifaforat pseudonomi oaktat kvasinera, antisiligen.
+              Görad krol som vasade: nuse, fast mönde ladat. Däning kontralöst,
+              misosk kahint därför att eurogov. Pretäda astrossa, plus kvasihon.
+            </p>
+            <Link href="/foro" className={styles.link}>
+              <span className={styles.text_link}>Foro</span>
+              <button className={styles.arrow_btn}></button>
+            </Link>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
---
+++ src/components/Carrusel/Carrusel.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Carrusel = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Carrusel
