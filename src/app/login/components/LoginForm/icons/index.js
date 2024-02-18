b5c55114ccda503e63b7a5869d93d4f87dce0be7
Diego-Fdez <dieghoxcr7@gmail.com>
2024-02-18 10:35:22 -0600

    muevo archivos a una nueva ruta

--- src/app/login/components/LoginForm/icons/index.js
+++
@@ -1,4 +0,0 @@
-export { default as CheckIcon } from './checkIcon';
-export { default as EyeIcon } from './eyeIcon';
-export { default as HiddenEyeIcon } from './hiddenEyeIcon';
-export { default as GoogleIcon } from './googleIcon';
