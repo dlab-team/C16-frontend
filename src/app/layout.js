fdef47d64b5707ca7ead280fde97b55efa44a974
ingrid-mo <morales.ingrid.i20@gmail.com>
2024-02-19 21:37:01 -0300

    solicion 1 rama

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,10 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body>
-        {/* <NavView />
+        <NavView />
         <>{children}</>
-        <Footer></Footer> */}
-        <LoginForm/>
+        <Footer></Footer> 
       </body>
     </html>
   );
