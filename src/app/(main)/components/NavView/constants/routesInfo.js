7b7b35f7a3bbbe05e9950a896d8d15a5de0ebfc8
Diego-Fdez <dieghoxcr7@gmail.com>
2024-02-23 13:31:39 -0600

    remove icons

--- src/app/(main)/components/NavView/constants/routesInfo.js
+++ src/app/(main)/components/NavView/constants/routesInfo.js
@@ -1,5 +1,3 @@
-import { HomeIcon } from '../icons';
-
 /**
  * This code defines an array called ROUTES_INFO that contains information about different paths in an application.
  * Each object in the array represents a path and has properties such as id, pathname, title, and icon.
@@ -9,36 +7,30 @@ export const ROUTES_INFO = [
     id: 1,
     pathname: '/',
     title: 'Inicio',
-    icon: <HomeIcon />, //ToDo: It has an icon?
   },
   {
     id: 2,
     pathname: '/quienessomos',
     title: 'Quienes Somos',
-    icon: <HomeIcon />, //ToDo: It has an icon?
   },
   {
     id: 3,
     pathname: '/programa',
     title: 'Programa',
-    icon: <HomeIcon />, //ToDo: It has an icon?
   },
   {
     id: 4,
     pathname: '/academia',
     title: 'Academia',
-    icon: <HomeIcon />, //ToDo: It has an icon?
   },
   {
     id: 5,
     pathname: '/recursosdeapoyo',
     title: 'Recursos de apoyo',
-    icon: <HomeIcon />, //ToDo: It has an icon?
   },
   {
     id: 6,
     pathname: '/foro',
     title: 'Foro',
-    icon: <HomeIcon />, //ToDo: It has an icon?
   },
 ];
