f65675dfcbaadc33c271d01ebec7ac7647455c7f
Diego-Fdez <dieghoxcr7@gmail.com>
2024-02-24 17:36:50 -0600

    add new styles

--- src/app/(main)/components/NavView/NavView.jsx
+++ src/app/(main)/components/NavView/NavView.jsx
@@ -5,21 +5,24 @@ import styles from './styles/NavView.module.css';
 import { ROUTES_INFO } from './constants';
 import { MenuIcon } from './icons';
 import { useNavView } from './hook';
+import { LoginButton } from './components';
 
 const NavView = () => {
   const { pathname, toggleMenu } = useNavView();
 
   return (
     <header className={styles.navContainer}>
-      <Image
-        src="/assets/logo-ronda.svg"
-        alt="logo Ronda"
-        width={53}
-        height={53}
-        loading="lazy"
-        className={styles.logo}
-      />
-      <nav id="navContainer" className={styles.nav}>
+      <Link href='/' className={styles.logoContainer}>
+        <Image
+          src='/assets/logo-ronda.svg'
+          alt='logo Ronda'
+          width={53}
+          height={53}
+          loading='lazy'
+          className={styles.logo}
+        />
+      </Link>
+      <nav id='navContainer' className={styles.nav}>
         {ROUTES_INFO.map((route) => (
           <Link
             className={`${styles.link} ${
@@ -29,22 +32,12 @@ const NavView = () => {
             onClick={toggleMenu}
             key={route.id}
           >
-            {/* ToDo: Do all items have an icon or none? */}
-            {/* {pathname === route.pathname && route.icon} */}
             {route.title}
           </Link>
         ))}
-        <Image
-          src="https://cdn.pixabay.com/photo/2017/11/29/09/15/paint-2985569_1280.jpg"
-          alt="user profile"
-          width={30}
-          height={30}
-          loading="lazy"
-          className={styles.profileImage}
-        />
       </nav>
       <div className={styles.buttonsContainer}>
-        <button className={styles.loginButton}>Ingresar</button>
+        <LoginButton />
         <button onClick={toggleMenu} className={styles.menuButton}>
           <MenuIcon />
         </button>
