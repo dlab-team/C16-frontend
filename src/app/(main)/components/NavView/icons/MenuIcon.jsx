e1530f12d292d362a2370e8a3c48c756b0d4c4a1
Diego-Fdez <dieghoxcr7@gmail.com>
2024-02-24 17:36:18 -0600

    create icons

--- src/app/(main)/components/NavView/icons/MenuIcon.jsx
+++ src/app/(main)/components/NavView/icons/MenuIcon.jsx
@@ -1,31 +1,32 @@
 const MenuIcon = () => (
   <svg
-    width="43"
-    height="38"
-    viewBox="0 0 43 38"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
+    width='45'
+    height='100%'
+    viewBox='0 0 43 38'
+    fill='none'
+    preserveAspectRatio='none'
+    xmlns='http://www.w3.org/2000/svg'
   >
     <path
-      d="M5.375 19H37.625"
-      stroke="black"
-      strokeWidth="3"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      d='M5.375 19H37.625'
+      stroke='black'
+      strokeWidth='3'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     />
     <path
-      d="M5.375 9.5H37.625"
-      stroke="black"
-      strokeWidth="3"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      d='M5.375 9.5H37.625'
+      stroke='black'
+      strokeWidth='3'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     />
     <path
-      d="M5.375 28.5H37.625"
-      stroke="black"
-      strokeWidth="3"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      d='M5.375 28.5H37.625'
+      stroke='black'
+      strokeWidth='3'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     />
   </svg>
 );
