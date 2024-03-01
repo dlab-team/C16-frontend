a8679f669f288a061f0fb284d4cf89748ed198c6
Diego-Fdez <dieghoxcr7@gmail.com>
2024-03-01 15:48:26 -0600

    change icons

--- src/app/(main)/components/NavView/icons/MenuIcon.jsx
+++ src/app/(main)/components/NavView/icons/MenuIcon.jsx
@@ -1,29 +1,28 @@
 const MenuIcon = () => (
   <svg
-    width='45'
-    height='100%'
-    viewBox='0 0 43 38'
+    width='44'
+    height='38'
+    viewBox='0 0 44 38'
     fill='none'
-    preserveAspectRatio='none'
     xmlns='http://www.w3.org/2000/svg'
   >
     <path
-      d='M5.375 19H37.625'
-      stroke='black'
+      d='M5.875 19H38.125'
+      stroke='#454444'
       strokeWidth='3'
       strokeLinecap='round'
       strokeLinejoin='round'
     />
     <path
-      d='M5.375 9.5H37.625'
-      stroke='black'
+      d='M5.875 9.5H38.125'
+      stroke='#454444'
       strokeWidth='3'
       strokeLinecap='round'
       strokeLinejoin='round'
     />
     <path
-      d='M5.375 28.5H37.625'
-      stroke='black'
+      d='M5.875 28.5H38.125'
+      stroke='#454444'
       strokeWidth='3'
       strokeLinecap='round'
       strokeLinejoin='round'
