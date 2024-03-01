0b08332119a667bc3c70250716d277d873912747
Diego-Fdez <dieghoxcr7@gmail.com>
2024-03-01 15:47:14 -0600

    update barrel

--- src/app/(main)/components/NavView/icons/index.js
+++ src/app/(main)/components/NavView/icons/index.js
@@ -1,3 +1,4 @@
+export { default as CloseIcon } from './CloseIcon';
 export { default as MenuIcon } from './MenuIcon';
 export { default as ProfileDesktopIcon } from './ProfileDesktopIcon';
 export { default as ProfileMobileIcon } from './ProfileMobileIcon';
