a582a6a52d8298832763c0de6086f43730f12bbd
ingrid-mo <morales.ingrid.i20@gmail.com>
2024-03-01 22:11:38 -0300

    modifique registro paso 1 , agregue a page

---
+++ src/app/(auth)/auth/componets/component_register_II/RegistroComponenteII.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import React from "react";
+import "./Style.css";
+
+const RegistroComponenteII = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RegistroComponenteII
