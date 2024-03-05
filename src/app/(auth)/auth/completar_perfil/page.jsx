5c4158fc38403f56eae2c2dc3b19640ebfde21be
ingrid-mo <morales.ingrid.i20@gmail.com>
2024-03-05 20:35:58 -0300

    realizé completa tu perfil

---
+++ src/app/(auth)/auth/completar_perfil/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import RegistroComponenteII from '../componets/component_register_II/RegistroComponenteII'
+const page = () => {
+  return (
+    <div>
+      <RegistroComponenteII/>
+    </div>
+  )
+}
+
+export default page
