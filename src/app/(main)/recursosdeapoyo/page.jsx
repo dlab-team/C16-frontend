881dc586b9eb7cb1079b4cb264161209bcd33bee
Diego-Fdez <dieghoxcr7@gmail.com>
2024-03-06 16:19:18 -0600

    add Tabs component

--- src/app/(main)/recursosdeapoyo/page.jsx
+++ src/app/(main)/recursosdeapoyo/page.jsx
@@ -1,10 +1,11 @@
+import { Tabs } from './components'
 
 function RecursosDeApoyo() {
-    return (
-        <div>
-            <h1 style={{textAlign: "center", padding:"20px"}}>RecursosDeApoyo page</h1>
-        </div>
-    )
+  return (
+    <main>
+      <Tabs />
+    </main>
+  )
 }
 
 export default RecursosDeApoyo
