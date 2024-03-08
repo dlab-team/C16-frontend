bb4d612027176426c43c713c3a8ea7f5d5b427c7
Diego-Fdez <dieghoxcr7@gmail.com>
2024-03-08 12:34:02 -0600

    add pagination component

--- src/app/(main)/recursosdeapoyo/page.jsx
+++ src/app/(main)/recursosdeapoyo/page.jsx
@@ -1,10 +1,14 @@
+import { PaginationView } from './components';
 
 function RecursosDeApoyo() {
-    return (
-        <div>
-            <h1 style={{textAlign: "center", padding:"20px"}}>RecursosDeApoyo page</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center', padding: '20px' }}>
+        RecursosDeApoyo page
+      </h1>
+      <PaginationView />
+    </div>
+  );
 }
 
-export default RecursosDeApoyo
+export default RecursosDeApoyo;
