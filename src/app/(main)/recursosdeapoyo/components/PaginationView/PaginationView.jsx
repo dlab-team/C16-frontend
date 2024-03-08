01b50d3f48f8e5ea6995f4ced4e0b8e5852a6fe7
Diego-Fdez <dieghoxcr7@gmail.com>
2024-03-08 14:23:32 -0600

    create pagination component

--- src/app/(main)/recursosdeapoyo/components/PaginationView/PaginationView.jsx
+++ src/app/(main)/recursosdeapoyo/components/PaginationView/PaginationView.jsx
@@ -1,19 +1,26 @@
-import styles from './styles/PaginationView.module.css';
+import { useResources } from '../../hooks'
+import styles from './styles/PaginationView.module.css'
 
 const PaginationView = () => {
+  const { paginationOptions, currentPage, handlePageChange, getTotalPages } =
+    useResources()
+
   return (
     <div className={styles.container}>
       <p className={styles.p}>Pag</p>
       <div className={styles.selectContainer}>
-        <select className={styles.select}>
-          <option value='1'>1</option>
-          <option value='2'>2</option>
+        <select
+          className={styles.select}
+          value={currentPage}
+          onChange={handlePageChange}
+        >
+          {paginationOptions.map((option) => option)}
         </select>
         <i className={styles.i} />
       </div>
-      <p className={styles.p}>de 10</p>
+      <p className={styles.p}>de {getTotalPages()}</p>
     </div>
-  );
-};
+  )
+}
 
-export default PaginationView;
+export default PaginationView
