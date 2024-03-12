c9c812655c9269f159906e57b9f835b0c7315a06
ingrid-mo <morales.ingrid.i20@gmail.com>
2024-03-11 23:21:16 -0300

    modifique los logos del nav y footer, ademas de los iconos del footer

--- src/app/(main)/components/FooterComponet/Footer.jsx
+++ src/app/(main)/components/FooterComponet/Footer.jsx
@@ -1,18 +1,44 @@
-import "./Styles.css";
+import './Styles.css'
+import Image from 'next/image'
+import {
+  AiOutlineEnvironment,
+  AiOutlineMail,
+  AiOutlineMobile,
+  AiFillFacebook,
+  AiFillInstagram,
+  AiOutlineTwitter,
+  AiFillYoutube,
+} from 'react-icons/ai'
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="footer__title">
         <div className="footer__body">
-          <img src="https://firebasestorage.googleapis.com/v0/b/tailsywhiskers.appspot.com/o/logo.svg?alt=media&token=516ded09-7338-4784-aa21-0e56cce2bf2d" alt="logo" />
-
+          <div className="footer__icon__mobile">
+            <Image
+              src="https://firebasestorage.googleapis.com/v0/b/c16-ronda.appspot.com/o/iconos%2FlogoMobile.png?alt=media&token=ef7b53bb-9d9a-4f64-9cfe-f56454001dbf"
+              alt="logo red nacional de cuidadores"
+              width={120}
+              height={120}
+              loading="lazy"
+            />
+          </div>
+          <div className="footer__icon__web">
+            <Image
+              src="https://firebasestorage.googleapis.com/v0/b/c16-ronda.appspot.com/o/iconos%2FRNC_Horizontal_Color.png?alt=media&token=7d7203c1-39ce-4e3f-b560-e7194ea0f8ac"
+              alt="logo red nacional de cuidadores"
+              width={320}
+              height={120}
+              loading="lazy"
+            />
+          </div>
 
           <div className="footer__body__contact">
             <div className="footer__body__contact__icon">
-              
-              <img src="https://firebasestorage.googleapis.com/v0/b/tailsywhiskers.appspot.com/o/map.svg?alt=media&token=b1936fc2-8682-46ca-a10b-bbe583bf811a" alt="map" />
-
+              <div className="icon">
+                <AiOutlineEnvironment />
+              </div>
             </div>
 
             <div className="footer__text">
@@ -22,7 +48,9 @@ const Footer = () => {
 
           <div className="footer__body__contact">
             <div className="footer__body__contact__icon">
-          <img src="https://firebasestorage.googleapis.com/v0/b/c16-ronda.appspot.com/o/iconos%2FEnviar.svg?alt=media&token=408ca3b6-4ebd-42db-bbb6-8d34175ea9d2" alt="enviar" />
+              <div className="icon">
+                <AiOutlineMail />
+              </div>
             </div>
 
             <div className="footer__text">
@@ -32,7 +60,9 @@ const Footer = () => {
 
           <div className="footer__body__contact">
             <div className="footer__body__contact__icon">
-<img src="https://firebasestorage.googleapis.com/v0/b/c16-ronda.appspot.com/o/iconos%2FTelefono.svg?alt=media&token=57b5ff91-0f6e-4a7e-8de0-bc774083272c" alt="telefono" />
+              <div className="icon">
+                <AiOutlineMobile />
+              </div>
             </div>
 
             <div className="footer__text">
@@ -42,19 +72,24 @@ const Footer = () => {
         </div>
         <div className="footer__logo">
           <a href="">
-     
-     <img src="https://firebasestorage.googleapis.com/v0/b/c16-ronda.appspot.com/o/iconos%2FFacebook.svg?alt=media&token=d8cbf50c-6f90-4b46-96f6-30d7171f92a2" alt="facebook" />
+            <div className="icon">
+              <AiFillFacebook />
+            </div>
           </a>
           <a href="">
-          <img src="https://firebasestorage.googleapis.com/v0/b/c16-ronda.appspot.com/o/iconos%2FInstagram.svg?alt=media&token=af05514b-4f2f-4e8e-9052-77a4e96bcff5" alt="instagram" />
+            <div className="icon">
+              <AiFillInstagram />
+            </div>
           </a>
           <a href="">
-            <img src="https://firebasestorage.googleapis.com/v0/b/c16-ronda.appspot.com/o/iconos%2FTwiiter.svg?alt=media&token=c89be9c2-373f-4430-8f7c-29707fa59cd9" alt="twiiter" />
-
+            <div className="icon">
+              <AiOutlineTwitter />
+            </div>
           </a>
           <a href="">
-            <img src="https://firebasestorage.googleapis.com/v0/b/c16-ronda.appspot.com/o/iconos%2FYoutube.svg?alt=media&token=1b03de35-d84b-4bad-a370-7178b2e725ee" alt="youtube" />
-
+            <div className="icon">
+              <AiFillYoutube />
+            </div>
           </a>
         </div>
         <div className="footer__line">
@@ -68,8 +103,10 @@ const Footer = () => {
 
           <div className="footer__footer__logo">
             <a href="">
-              <img src="https://firebasestorage.googleapis.com/v0/b/c16-ronda.appspot.com/o/imagenes%2FlogoDesafioLatam.png?alt=media&token=bba247fc-7c12-4bf7-b712-e52140c23e5f" alt="desafio latam" />
-             
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/c16-ronda.appspot.com/o/imagenes%2FlogoDesafioLatam.png?alt=media&token=bba247fc-7c12-4bf7-b712-e52140c23e5f"
+                alt="desafio latam"
+              />
             </a>
             <div>
               <p>2024</p>
@@ -78,7 +115,7 @@ const Footer = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
