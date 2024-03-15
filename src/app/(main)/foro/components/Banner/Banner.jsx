d4b6340a1443b5d30e58da03e7ed652635aece29
manuel14mds <manuel14mds@gmail.com>
2024-03-15 16:35:03 -0500

    finish publication style

--- src/app/(main)/foro/components/Banner/Banner.jsx
+++ src/app/(main)/foro/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import styles from './Banner.module.css'
 
 function Banner() {
     return (
-        <div className={styles.banner} role='banner'>
+        <section className={styles.banner} role='banner'>
             <Image width={100} height={100} alt='banner image' src='/assets/images/foro/banner_img.png' className={styles.bannerImg}/>
             <div className={styles.bannerTextBox}>
                 <div className={styles.textBox}>
@@ -11,7 +11,7 @@ function Banner() {
                     <span className={styles.bannerMessage}>Qorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
