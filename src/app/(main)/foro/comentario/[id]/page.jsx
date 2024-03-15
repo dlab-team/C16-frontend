446ce5abecb102d1e2593f9bfba97090d2e0b016
manuel14mds <manuel14mds@gmail.com>
2024-03-15 16:35:03 -0500

    finish publication style

--- src/app/(main)/foro/comentario/[id]/page.jsx
+++ src/app/(main)/foro/comentario/[id]/page.jsx
@@ -1,11 +1,27 @@
+import Post from "../../components/Post/Post"
+import styles from "./comment.module.css"
 
+import Breadcumbs from "./components/Breadcumbs/Breadcumbs"
+
+const data = { id:0, nombre: "Diego Fernández", region: "costa Rica", fecha: "2/2/2024", mensaje: "¿Cómo puedo tramitar el Carné de Cuidador? Gracias", imagen: "" }
 
 function Comment({params}) {
     const {id}= params
-    console.log('ID:', id)
+    data.id = id
+
     return (
-        <main>
-            comentarios
+        <main className={styles.main}>
+            <section className={styles.container}>
+                <Breadcumbs />
+
+                <h2 className={styles.subTitle}>Comentarios</h2>
+            </section>
+
+            <section className={styles.postBox}>
+                <Post data={data} type='detail' />
+            </section>
+
+            comentarios:{id}
         </main>
     )
 }
