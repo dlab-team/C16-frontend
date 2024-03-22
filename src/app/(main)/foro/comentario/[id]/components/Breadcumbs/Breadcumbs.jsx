6e39097cdf8eb8c6b60796c75d6c7c61b0a63fb7
manuel14mds <manuel14mds@gmail.com>
2024-03-21 19:02:09 -0500

    adding new features: search bar, change CSS custom properties, update forum style

--- src/app/(main)/foro/comentario/[id]/components/Breadcumbs/Breadcumbs.jsx
+++ src/app/(main)/foro/comentario/[id]/components/Breadcumbs/Breadcumbs.jsx
@@ -3,12 +3,12 @@
 import { useRouter } from 'next/navigation'
 import styles from './Breadcumbs.module.css'
 
-function Breadcumbs() {
+function Breadcumbs({message= "Foro"}) {
     const router = useRouter()
     return (
         <button className={styles.breadcumbs} onClick={() => router.back()}>
             <div className={styles.arrowIcon}></div>
-            <span className={styles.breadcumbsTitle}>Foro</span>
+            <span className={styles.breadcumbsTitle}>{message}</span>
         </button>
     )
 }
