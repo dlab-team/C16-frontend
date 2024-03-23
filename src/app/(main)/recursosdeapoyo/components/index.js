b6bfa4d59506ab09b1349d0819d294894e844c8c
Diego-Fdez <dieghoxcr7@gmail.com>
2024-03-22 19:11:56 -0600

    update barrel

--- src/app/(main)/recursosdeapoyo/components/index.js
+++ src/app/(main)/recursosdeapoyo/components/index.js
@@ -1,3 +1,3 @@
-export { default as Tabs } from './Tabs/Tabs'
-export { default as PaginationView } from './PaginationView/PaginationView'
+export { default as ContainerView } from './ContainerView/ContainerView'
 export { default as ResourcesTopView } from './ResourcesTopView/ResourcesTopView'
+export { default as Tabs } from './Tabs/Tabs'
