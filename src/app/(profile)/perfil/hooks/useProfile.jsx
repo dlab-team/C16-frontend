14f140cb36ffd8087df289fb162a0eee2160f525
Diego-Fdez <dieghoxcr7@gmail.com>
2024-04-08 18:22:16 -0600

    all function anda data for profile page

--- src/app/(profile)/perfil/hooks/useProfile.jsx
+++ src/app/(profile)/perfil/hooks/useProfile.jsx
@@ -5,13 +5,20 @@ import {
   AiTwotoneAlert,
   AiOutlineLogout,
 } from 'react-icons/ai'
-import { YourDataView } from '../components'
+import {
+  ChangePasswordView,
+  NotificationsView,
+  YourDataView,
+} from '../components'
 import { useWindowDimensions } from '.'
 
 const useProfile = () => {
   const { width } = useWindowDimensions()
   const [componentSelected, setComponentSelected] = useState(0)
   const [showMainMenu, setShowMainMenu] = useState(true)
+  const [read, setRead] = useState(false)
+  const [optionSelected, setOptionSelected] = useState(1)
+  const [paginationItems, setPaginationItems] = useState([])
 
   const BUTTONS_NAMES = [
     {
@@ -36,6 +43,7 @@ const useProfile = () => {
     },
   ]
 
+  //Function to toggle the main menu if the selected component is not 0 and the width is less than 920px
   function toggleMainMenu() {
     if (componentSelected !== 0 && width < 920) setShowMainMenu(!showMainMenu)
   }
@@ -44,13 +52,54 @@ const useProfile = () => {
     toggleMainMenu()
   }, [componentSelected, width])
 
+  //A function that handles the selection of a component and sets it as selected and selected option.
   function handleSelectComponentId(id) {
     setComponentSelected(id)
+    setOptionSelected(id)
   }
 
+  //return the total pages
+  function getTotalPages() {
+    return 1
+  }
+
+  /*The paginationOptions function generates a list of options for pagination based on the total number of pages.
+Each option has a numeric value corresponding to the page number. */
+  function paginationOptions() {
+    const totalPages = getTotalPages()
+    const options = []
+    for (let i = 1; i <= totalPages; i++) {
+      options.push(
+        <option key={i} value={i}>
+          {i}
+        </option>,
+      )
+    }
+    setPaginationItems(options)
+  }
+
+  useEffect(() => {
+    paginationOptions()
+  }, [])
+
+  function handlePageChange() {}
+
+  /*A function that renders a specific component based on the selection of a component. 
+  If no component is selected, a default component is rendered. */
   function renderRightComponent() {
     const RIGHT_COMPONENT = {
       1: <YourDataView />,
+      2: <ChangePasswordView />,
+      3: (
+        <NotificationsView
+          read={read}
+          setRead={setRead}
+          paginationOptions={paginationItems}
+          currentPage={1}
+          handlePageChange={handlePageChange}
+          getTotalPages={getTotalPages}
+        />
+      ),
     }
 
     const DEFAULT_COMPONENT = <YourDataView />
@@ -60,11 +109,19 @@ const useProfile = () => {
     return component
   }
 
+  //show main menu
+  function handleGoBack() {
+    setShowMainMenu(!showMainMenu)
+    setComponentSelected(0)
+  }
+
   return {
     BUTTONS_NAMES,
     handleSelectComponentId,
     renderRightComponent,
     showMainMenu,
+    handleGoBack,
+    optionSelected,
   }
 }
 
