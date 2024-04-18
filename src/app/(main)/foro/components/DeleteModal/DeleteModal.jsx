0c022a7f58e3a3b4b47ee3e8d9f33711bc55d608
manuel14mds <manuel14mds@gmail.com>
2024-04-18 13:29:41 -0500

    add post delete modal

---
+++ src/app/(main)/foro/components/DeleteModal/DeleteModal.jsx
@@ -0,0 +1,27 @@
+import styles from './DeleteModal.module.css'
+
+const DeleteModal = ({ isOpen, onClose, onConfirm }) => {
+    if (!isOpen) return null
+
+    return (
+        <div className={styles.modalOverlay} onClick={onClose}>
+            <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
+                <div className={styles.header}>
+                    <button className={styles.closeBtn} onClick={onClose}></button>
+                </div>
+
+                <div className={styles.modalInfo}>
+                    <h3 className={styles.title}>Eliminar Comentario</h3>
+                    <p className={styles.message}>Estás seguro que deseas reportar eliminar el comentario seleccionado?</p>
+                </div>
+
+                <div className={styles.modalActions}>
+                    <button className={styles.closeButton} onClick={onClose}>Cancelar</button>
+                    <button className={styles.reportButton} onClick={onConfirm}>Eliminar</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DeleteModal
--- src/app/(main)/foro/components/MoreDropdown/MoreDropdown.jsx
+++ src/app/(main)/foro/components/MoreDropdown/MoreDropdown.jsx
@@ -1,26 +1,47 @@
 'use client'
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from "@nextui-org/react";
 import styles from './MoreDropdown.module.css'
-function MoreDropdown() {
+import { useState } from "react";
+import ModalPortal from "../ModalPortal/ModalPortal";
+import DeleteModal from "../DeleteModal/DeleteModal";
+
+function MoreDropdown({data}) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+    
+    const deletePost=()=>{
+
+        alert(`el comentario ${data} ha sido eliminado`)
+    }
+
     return (
-        <Dropdown>
-            <DropdownTrigger>
-                <Button
-                    variant="bordered"
-                    className={styles.moreBtn}
-                />
-            </DropdownTrigger>
-            <DropdownMenu aria-label="Static Actions" className={styles.menu} id="dropdown_menu_ul">
-                <DropdownItem key="edit" className={styles.dropOption}>
-                    <div className={styles.penIcon}></div>
-                    <span className={styles.optionText}>Editar comentario</span>
-                </DropdownItem>
-                <DropdownItem key="delete" className={styles.dropOption}>
-                    <div className={styles.closeIcon}></div>
-                    <span className={styles.optionText}>Eliminar comentario</span>
-                </DropdownItem>
-            </DropdownMenu>
-        </Dropdown>
+        <>
+            <Dropdown>
+                <DropdownTrigger>
+                    <Button
+                        variant="bordered"
+                        className={styles.moreBtn}
+                        />
+                </DropdownTrigger>
+                <DropdownMenu aria-label="Static Actions" className={styles.menu} id="dropdown_menu_ul">
+                    <DropdownItem key="edit" className={styles.dropOption}>
+                        <div className={styles.penIcon}></div>
+                        <span className={styles.optionText}>Editar comentario</span>
+                    </DropdownItem>
+                    <DropdownItem key="delete" className={styles.dropOption} onClick={openModal}>
+                        <div className={styles.closeIcon}></div>
+                        <span className={styles.optionText}>Eliminar comentario</span>
+                    </DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
+            
+
+            <ModalPortal>
+                <DeleteModal isOpen={isModalOpen} onClose={closeModal} onConfirm={deletePost}/>
+            </ModalPortal>
+        </>
     )
 }
 
