7d78425d57872048a92f5aa7cd20db8007c262bb
manuel14mds <manuel14mds@gmail.com>
2024-04-18 21:40:23 -0500

    update completarPerfil componet, fix datapicker, fix style, fix form validation

--- src/app/(auth)/auth/completarPerfil/page.jsx
+++ src/app/(auth)/auth/completarPerfil/page.jsx
@@ -1,23 +1,11 @@
-import React from 'react'
 import CompletPerfil from '../componets/CompleteProfile/CompletPerfil'
 import "./Styles.css";
 
 const page = () => {
-  
   return (
-  
-    
-  <>
-    <div className='background'> 
-     <img className='img__l' src="https://firebasestorage.googleapis.com/v0/b/c16-ronda.appspot.com/o/imagenes%2Fmanos-unidas-registro-l.png?alt=media&token=c591bc19-5a3c-4824-a274-382c35009adc" alt="manos unidas" /> 
-     <img className='img__xl' src="https://firebasestorage.googleapis.com/v0/b/c16-ronda.appspot.com/o/imagenes%2Fmanos-unidas-registro-lI.png?alt=media&token=4d8e1556-4037-46f3-89a0-e93de34aa788" alt="manos unidas" />
-      </div>
-      <div className='register__componet'>
+    <main className='background'>
       <CompletPerfil />
-      </div>
-      </>
-
-  
+    </main>
   )
 }
 
