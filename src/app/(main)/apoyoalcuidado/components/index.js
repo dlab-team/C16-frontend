d3cb849308674680302da16b64536c4d78a116e2
Diego-Fdez <dieghoxcr7@gmail.com>
2024-04-26 19:32:11 -0600

    update barrel

--- src/app/(main)/apoyoalcuidado/components/index.js
+++ src/app/(main)/apoyoalcuidado/components/index.js
@@ -1,3 +1,4 @@
 export { default as ContainerView } from './ContainerView/ContainerView'
 export { default as ResourcesTopView } from './ResourcesTopView/ResourcesTopView'
 export { default as Tabs } from './Tabs/Tabs'
+export { default as FiltersView } from './FiltersView/FiltersView'
