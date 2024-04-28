320f609e83b8749fdad5b433c1b95b590185d965
Diego-Fdez <dieghoxcr7@gmail.com>
2024-04-27 18:48:01 -0600

    agrego componente de recurso destacado

--- src/app/(main)/apoyoalcuidado/components/Tabs/Tabs.jsx
+++ src/app/(main)/apoyoalcuidado/components/Tabs/Tabs.jsx
@@ -1,11 +1,12 @@
 import { FiltersView } from '..'
-import { TabCards } from './components'
+import { LeadingResourceCard, TabCards } from './components'
 import styles from './styles/Tabs.module.css'
 
 const Tabs = () => {
   return (
     <section className={styles.container}>
       <FiltersView />
+      <LeadingResourceCard />
       <TabCards />
     </section>
   )
