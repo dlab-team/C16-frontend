a99f7b2a466bd86dfbba399ffedd0781f5c58cc5
Manuel Florez <89274119+manuel14mds@users.noreply.github.com>
2024-04-29 11:12:53 -0500

    E1-HU2-FE2-Style/MFlorez (#94)
    * add generic banner component
    
    * update home banner style
    
    * add: login reactive banner, styling loading page update

--- src/app/(main)/foro/layout.jsx
+++ src/app/(main)/foro/layout.jsx
@@ -1,11 +1,17 @@
-import Banner from "./components/Banner/Banner"
+import GenericBanner from "../components/GenericBanner/GenericBanner"
 import styles from './Foro.module.css'
 
+const dataBanner = {
+    imgUrl:"https://firebasestorage.googleapis.com/v0/b/c16-ronda.appspot.com/o/imagenes%2FimgForo2.png?alt=media&token=aa4095cc-47f5-4c4e-9854-7921c7f07eac",
+    titleMessage:'Participa en nuestro', //mensaje que va en el titulo
+    titleEmphasis:'Foro', // el enfasis del texto que va color azul
+    message:'Qorem ipsum dolor sit amet, consectetur adipiscing elit.', //mensaje del banner
+}
 
 function layout({ children }) {
     return (
         <main className={styles.main}>
-            <Banner />
+            <GenericBanner resource={dataBanner}/>
             <>{children}</>
         </main>
     )
