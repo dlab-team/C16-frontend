d19eeea2834950c38f33d2516986d85ff3067a15
Diego-Fdez <dieghoxcr7@gmail.com>
2024-04-28 19:09:10 -0600

    create page about us

--- src/app/(main)/quienessomos/page.jsx
+++ src/app/(main)/quienessomos/page.jsx
@@ -1,25 +1,70 @@
-'use client';
+import DinamicBanner from '../components/DinamicBanner/DinamicBanner'
+import styles from '../components/DinamicBanner/DinamicBanner.module.css'
+import containerStyles from './styles/Page.module.css'
+import { Carousel, UsView } from './components'
 
-import React, { useState, useEffect } from "react";
-import DinamicBanner from "../components/DinamicBanner/DinamicBanner";
-import styles from "../components/DinamicBanner/DinamicBanner.module.css";
+// Datos de ejemplo para las imágenes del carrusel
+export const imagenesDeEjemplo = [
+  {
+    id: 1,
+    src: '/assets/images/banner/enacionaldecuidado.png',
+    alt: 'Imagen 1',
+    url: 'http://direccion1.com',
+    name: 'RNCuidadoras',
+  },
+  {
+    id: 2,
+    src: '/assets/images/banner/enacionaldecuidado.png',
+    alt: 'Imagen 1',
+    url: 'http://direccion1.com',
+    name: 'RNCuidadoras',
+  },
+  {
+    id: 3,
+    src: '/assets/images/banner/enacionaldecuidado.png',
+    alt: 'Imagen 1',
+    url: 'http://direccion1.com',
+    name: 'RNCuidadoras',
+  },
+  {
+    id: 4,
+    src: '/assets/images/banner/enacionaldecuidado.png',
+    alt: 'Imagen 1',
+    url: 'http://direccion1.com',
+    name: 'RNCuidadoras',
+  },
+  {
+    id: 5,
+    src: '/assets/images/banner/enacionaldecuidado.png',
+    alt: 'Imagen 1',
+    url: 'http://direccion1.com',
+    name: 'RNCuidadoras',
+  },
+]
 
-    function QuienesSomos() {
-        return (
-            <DinamicBanner
-                imageSrc='/assets/images/banner/quienessomosbanner.png'
-                title='Sobre Nosotros'
-                message= ''
-                customStyles={{
-                    desktopContainer: styles.quienesSomosDesktopContainer,
-                    bannerImg: styles.quienesSomosBannerImg,
-                    bannerTextBox: styles.quienesSomosBannerTextBox,
-                    bannerTitle: styles.quienesSomosBannerTitle,
-                    bannerMessage: styles.quienesSomosBannerMessage
-                }}
-            />
-        );
-    }
+function QuienesSomos() {
+  return (
+    <>
+      <DinamicBanner
+        imageSrc="/assets/images/banner/quienessomosbanner.png"
+        title="Sobre Nosotros"
+        message=""
+        customStyles={{
+          desktopContainer: styles.quienesSomosDesktopContainer,
+          bannerImg: styles.quienesSomosBannerImg,
+          bannerTextBox: styles.quienesSomosBannerTextBox,
+          bannerTitle: styles.quienesSomosBannerTitle,
+          bannerMessage: styles.quienesSomosBannerMessage,
+        }}
+      />
+      <UsView />
+      <div className={containerStyles.container}>
+        <h2 className={containerStyles.title}>Nuestra Red</h2>
+        <Carousel networkInfo={imagenesDeEjemplo} slideLeft={true} />
+        <Carousel networkInfo={imagenesDeEjemplo} slideLeft={false} />
+      </div>
+    </>
+  )
+}
 
-    export default QuienesSomos;
-
+export default QuienesSomos
