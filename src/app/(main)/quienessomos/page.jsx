b58d70eacb660e2f80d5ec7e75448967371371ed
Diego-Fdez <dieghoxcr7@gmail.com>
2024-04-29 18:08:01 -0600

    add generic banner

--- src/app/(main)/quienessomos/page.jsx
+++ src/app/(main)/quienessomos/page.jsx
@@ -1,41 +1,49 @@
-import DinamicBanner from '../components/DinamicBanner/DinamicBanner'
-import styles from '../components/DinamicBanner/DinamicBanner.module.css'
+import GenericBanner from '../components/GenericBanner/GenericBanner'
 import containerStyles from './styles/Page.module.css'
 import { Carousel, UsView } from './components'
+
+const dataBanner = {
+  imgUrl:
+    'https://firebasestorage.googleapis.com/v0/b/c16-ronda.appspot.com/o/imagenes%2FimgAboutus.png?alt=media&token=b9f7b84d-7e6c-4718-a417-1fa2aadca7d5',
+  titleMessage: '', //mensaje que va en el titulo
+  titleEmphasis: 'Sobre Nosotros', // el enfasis del texto que va color azul
+  message:
+    'Torem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit', //mensaje del banner
+}
 
 // Datos de ejemplo para las imágenes del carrusel
 export const imagenesDeEjemplo = [
   {
     id: 1,
-    src: '/assets/images/banner/enacionaldecuidado.png',
+    src: '/assets/img/logo.svg',
     alt: 'Imagen 1',
     url: 'http://direccion1.com',
     name: 'RNCuidadoras',
   },
   {
     id: 2,
-    src: '/assets/images/banner/enacionaldecuidado.png',
+    ssrc: '/assets/img/logo.svg',
     alt: 'Imagen 1',
     url: 'http://direccion1.com',
     name: 'RNCuidadoras',
   },
   {
     id: 3,
-    src: '/assets/images/banner/enacionaldecuidado.png',
+    src: '/assets/img/logo.svg',
     alt: 'Imagen 1',
     url: 'http://direccion1.com',
     name: 'RNCuidadoras',
   },
   {
     id: 4,
-    src: '/assets/images/banner/enacionaldecuidado.png',
+    src: '/assets/img/logo.svg',
     alt: 'Imagen 1',
     url: 'http://direccion1.com',
     name: 'RNCuidadoras',
   },
   {
     id: 5,
-    src: '/assets/images/banner/enacionaldecuidado.png',
+    src: '/assets/img/logo.svg',
     alt: 'Imagen 1',
     url: 'http://direccion1.com',
     name: 'RNCuidadoras',
@@ -44,26 +52,15 @@ export const imagenesDeEjemplo = [
 
 function QuienesSomos() {
   return (
-    <>
-      <DinamicBanner
-        imageSrc="/assets/images/banner/quienessomosbanner.png"
-        title="Sobre Nosotros"
-        message=""
-        customStyles={{
-          desktopContainer: styles.quienesSomosDesktopContainer,
-          bannerImg: styles.quienesSomosBannerImg,
-          bannerTextBox: styles.quienesSomosBannerTextBox,
-          bannerTitle: styles.quienesSomosBannerTitle,
-          bannerMessage: styles.quienesSomosBannerMessage,
-        }}
-      />
+    <main>
+      <GenericBanner resource={dataBanner} />
       <UsView />
-      <div className={containerStyles.container}>
+      <section className={containerStyles.container}>
         <h2 className={containerStyles.title}>Nuestra Red</h2>
         <Carousel networkInfo={imagenesDeEjemplo} slideLeft={true} />
         <Carousel networkInfo={imagenesDeEjemplo} slideLeft={false} />
-      </div>
-    </>
+      </section>
+    </main>
   )
 }
 
