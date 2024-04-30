5da617ad1d638296ba1e20829dcb8bb6e77d2fd4
Manuel Florez <89274119+manuel14mds@users.noreply.github.com>
2024-04-29 23:01:13 -0500

    Revert "E6-HU2-FE6-Feat/dFedez (#92)"
    This reverts commit 02f29ae0fecf92f9f831da516b158bfad1bd8bc7.

--- src/app/(main)/apoyoalcuidador/components/Tabs/components/index.js
+++ src/app/(main)/apoyoalcuidador/components/Tabs/components/index.js
@@ -1,3 +1,1 @@
 export { default as TabCards } from './Cards/TabCards'
-export { default as FiltersView } from './FiltersView/FiltersView'
-export { default as LeadingResourceCard } from './LeadingResourceCard/LeadingResourceCard'
