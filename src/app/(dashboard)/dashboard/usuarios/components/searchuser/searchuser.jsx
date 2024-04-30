9168d72503dcd37ea5cddf4a509bff38749a1b70
Felipe Burboa <felipeburboa1991@gmail.com>
2024-04-30 00:28:05 -0400

    feat: consuming user endpoints

--- src/app/(dashboard)/dashboard/usuarios/components/searchuser/searchuser.jsx
+++ src/app/(dashboard)/dashboard/usuarios/components/searchuser/searchuser.jsx
@@ -2,26 +2,28 @@ import { IoSearchOutline } from 'react-icons/io5'
 import { IoMdOptions } from 'react-icons/io'
 import styles from './styles/searchUser.module.css'
 
+const SearchUser = ({ setSearchValue, setPage }) => {
+  const handleInputChange = (event) => {
+    setSearchValue(event.target.value)
+    setPage(1)
+  }
 
-const SearchUser = () => {
   return (
     <div className={styles.search}>
       <div className={styles.searchSeccion}>
-      <div className={styles.searchButton}>
-        <button>
-          <IoSearchOutline />
-        </button>
-      </div>
-      <div className={styles.searchInput}>
-        <input type="text" placeholder='Ronda' />
-      </div>
-      <div className={styles.searchButton}>
-    
-        <button>
-     
-          <IoMdOptions />
-        </button>
-      </div>
+        <div className={styles.searchButton}>
+          <button>
+            <IoSearchOutline />
+          </button>
+        </div>
+        <div className={styles.searchInput}>
+          <input type="text" placeholder="Ronda" onChange={handleInputChange} />
+        </div>
+        <div className={styles.searchButton}>
+          <button>
+            <IoMdOptions />
+          </button>
+        </div>
       </div>
     </div>
   )
--- src/services/api/api.user.service.js
+++ src/services/api/api.user.service.js
@@ -27,6 +27,15 @@ const getUser = async (uid) => {
     return response.json()
 }
 
+// consulto todos los usuarios
+const getAllUsers = async (page = 1, name = "") => {
+    const response = await fetch(`https://c16-backend.onrender.com/api/users?page=${page}&name=${name}`)
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return response.json()
+}
 
 // Actualizo un los campos de un usuario por medio de su ID
 const updateUser = async (uid, newData, idToken) => {
@@ -48,8 +57,82 @@ const updateUser = async (uid, newData, idToken) => {
     return response.json()
 }
 
+const updateUserPhoto = async (uid, formData, idToken) => {
+
+    const response = await fetch(`https://c16-backend.onrender.com/api/users/${uid}/uploadUserImage`, {
+        method: "PATCH",
+        headers: {
+            Authorization: `Bearer ${idToken}`,
+        },
+        body: formData
+    })
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return response.json()
+}
+
+//Descargar excel de usuarios
+
+const downloadExcel = async (idToken) => {
+    try {
+        const response = await fetch(`https://c16-backend.onrender.com/api/users/downloadExcel`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${idToken}`,
+            },
+        });
+       return response
+
+    } catch (error) {
+        console.error('Error descargando el archivo:', error);
+    }
+}
+
+// elimino un usuario por medio de su ID
+const deleteUser = async (uid, idToken) => {
+    const response = await fetch(`https://c16-backend.onrender.com/api/users/${uid}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${idToken}`,
+        },
+    })
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    
+    return response
+}
+
+const disableUser = async (uid, idToken, enabled) => {
+    const response = await fetch(`https://c16-backend.onrender.com/api/users/${uid}/toggleEnabled`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${idToken}`,
+        },
+        body: JSON.stringify({enabled})
+    })
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    
+    return response.json()
+}
+
 export {
     createUser,
     getUser,
+    getAllUsers,
     updateUser,
+    downloadExcel,
+    updateUserPhoto,
+    deleteUser,
+    disableUser
 }
