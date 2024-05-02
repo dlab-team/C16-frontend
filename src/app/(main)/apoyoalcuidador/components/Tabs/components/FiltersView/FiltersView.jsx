62f7cdbfe2460eef01705c6f5f0776bba27a9c34
Diego-Fdez <dieghoxcr7@gmail.com>
2024-05-01 18:18:03 -0600

    fix filter view from apoyoalcuidador

--- src/app/(main)/apoyoalcuidador/components/Tabs/components/FiltersView/FiltersView.jsx
+++ src/app/(main)/apoyoalcuidador/components/Tabs/components/FiltersView/FiltersView.jsx
@@ -1,16 +1,57 @@
-import { RiEqualizerFill } from 'react-icons/ri'
+'use client'
+import { useState } from 'react'
+import { AiOutlineDown } from 'react-icons/ai'
+import regionesData from '../../../../../../(auth)/auth/componets/CompleteProfile/regionesData.json'
 import styles from './styles/FiltersView.module.css'
 
 const FiltersView = () => {
+  const [region, setRegion] = useState('')
+  const [comuna, setComuna] = useState('')
+
   return (
     <div className={styles.container}>
-      <div className={styles.inputContainer}>
-        <input type="search" placeholder="Región" className={styles.input} />
-        <RiEqualizerFill className={styles.icon} />
+      <div className={styles.selectContainer}>
+        <select
+          name="region"
+          id="region"
+          className={styles.select}
+          value={region}
+          onChange={(e) => {
+            setRegion(e.target.value)
+            setComuna('none')
+          }}
+        >
+          <option value="none">Selecciona</option>
+          {regionesData?.regiones.map((region, index) => (
+            <option key={index} value={region?.region}>
+              {region?.region}
+            </option>
+          ))}
+        </select>
+        <AiOutlineDown className={styles.icon} />
       </div>
-      <div className={styles.inputContainer}>
-        <input type="search" placeholder="Comuna" className={styles.input} />
-        <RiEqualizerFill className={styles.icon} />
+      <div className={styles.selectContainer}>
+        <select
+          name="comuna"
+          id="comuna"
+          value={comuna}
+          onChange={(e) => {
+            setComuna(e.target.value)
+          }}
+          disabled={region === 'none'}
+          className={styles.select}
+          required
+        >
+          <option value="none">Selecciona</option>
+          {regionesData.regiones
+            .find((r) => r?.region === region)
+            ?.comunas?.map((comuna, index) => (
+              <option key={index} value={comuna}>
+                {comuna}
+              </option>
+            ))}
+        </select>
+        <AiOutlineDown className={styles.icon} />
       </div>
     </div>
   )
