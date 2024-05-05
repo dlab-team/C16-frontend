1e7d39ce6c9fa27cd630914de9d2860225c7f1ce
Diego-Fdez <dieghoxcr7@gmail.com>
2024-05-05 17:48:19 -0600

    connect with backend

--- src/app/(dashboard)/dashboard/recursosdeapoyo/[id]/page.jsx
+++ src/app/(dashboard)/dashboard/recursosdeapoyo/[id]/page.jsx
@@ -1,11 +1,41 @@
+'use client'
 import { AiOutlineCheck } from 'react-icons/ai'
+import { PiUploadSimpleBold } from 'react-icons/pi'
 import Image from 'next/image'
 import { FiTrash } from 'react-icons/fi'
-import { GoBackButton, SaveButton, TitleView } from '../../components'
+import {
+  DeleteResourceModal,
+  GoBackButton,
+  SaveButton,
+  Spinner,
+  TitleView,
+} from '../../components'
 import mainStyles from './styles/PageId.module.css'
 import styles from '../components/ResourceView/styles/ResourceView.module.css'
+import { useDashboardResources } from '../hooks'
 
 const EditResource = () => {
+  const {
+    isLoading,
+    handleDeleteResourceById,
+    handleGoBack,
+    dialogIsOpen,
+    setDialogIsOpen,
+    description,
+    setDescription,
+    comuna,
+    setComuna,
+    url,
+    setUrl,
+    highlighted,
+    setHighlighted,
+    image,
+    setImage,
+    resourceId,
+    handleUpdateResourceById,
+    fileInputRef,
+  } = useDashboardResources()
+
   return (
     <section className={mainStyles.container}>
       <GoBackButton />
@@ -15,67 +45,116 @@ const EditResource = () => {
         showIcons={true}
         redirectTo="/dashboard/recursosdeapoyo/crear"
       />
-      <div className={mainStyles.wrapper}>
-        <div className={styles.container}>
-          <div className={styles.leftContainer}>
-            <Image
-              width={130}
-              height={95}
-              src="https://img.freepik.com/free-photo/programming-background-with-person-working-with-codes-computer_23-2150010125.jpg?size=626&ext=jpg"
-              alt="Resource"
-              className={styles.image}
-            />
-            <div className={styles.textContainer}>
-              <h5 className={styles.title}>Descripción</h5>
-              <textarea
-                name="description"
-                required
-                autoFocus
-                value={
-                  'Sorem ipsum dolor sit amet, consectetur adipiscing elit.'
-                }
-                className={mainStyles.description}
-              />
-              <label htmlFor="remember" className={styles.rememberInput}>
-                <div className={styles.checkboxContainer}>
-                  <input
-                    id="remember"
-                    name="remember"
-                    type="checkbox"
-                    className={styles.checkboxRound}
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <div className={mainStyles.wrapper}>
+            <div className={styles.container}>
+              <div className={styles.leftContainer}>
+                <div className={mainStyles.imageContainer}>
+                  <Image
+                    width={130}
+                    height={95}
+                    src={
+                      typeof image === 'string'
+                        ? image
+                        : URL.createObjectURL(image)
+                    }
+                    alt={description}
+                    className={styles.image}
                   />
-                  <span className={styles.span}>
-                    <AiOutlineCheck size={10} />
+                  <span className={mainStyles.span}>
+                    <PiUploadSimpleBold size={18} />
                   </span>
+                  <input
+                    type="file"
+                    accept="image/*"
+                    ref={fileInputRef}
+                    className={mainStyles.inputImage}
+                    onChange={(e) => setImage(e.target.files[0])}
+                  />
                 </div>
-                Destacar recurso
-              </label>
+                <div className={styles.textContainer}>
+                  <h5 className={styles.title}>Descripción</h5>
+                  <textarea
+                    name="description"
+                    required
+                    autoFocus
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    className={mainStyles.description}
+                  />
+                  <label htmlFor="remember" className={styles.rememberInput}>
+                    <div className={styles.checkboxContainer}>
+                      <input
+                        id="remember"
+                        name="remember"
+                        type="checkbox"
+                        className={styles.checkboxRound}
+                        checked={highlighted}
+                        onChange={() => setHighlighted(!highlighted)}
+                      />
+                      <span className={styles.span}>
+                        <AiOutlineCheck size={10} />
+                      </span>
+                    </div>
+                    Destacar recurso
+                  </label>
+                </div>
+              </div>
+              <div className={styles.rightContainer}>
+                <div className={styles.wrapper}>
+                  <h5 className={styles.title}>Comuna</h5>
+                  <input
+                    type="text"
+                    className={mainStyles.input}
+                    value={comuna}
+                    onChange={(e) => setComuna(e.target.value)}
+                  />
+                </div>
+                <div className={styles.wrapper}>
+                  <h5 className={styles.title}>Link</h5>
+                  <input
+                    type="text"
+                    className={mainStyles.link}
+                    value={url}
+                    onChange={(e) => setUrl(e.target.value)}
+                  />
+                </div>
+                <div className={styles.buttonContainer}>
+                  <button
+                    className={`${styles.button} ${styles.remove}`}
+                    onClick={() => setDialogIsOpen(!dialogIsOpen)}
+                  >
+                    <FiTrash size={20} />
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
-          <div className={styles.rightContainer}>
-            <div className={styles.wrapper}>
-              <h5 className={styles.title}>Comuna</h5>
-              <input
-                type="text"
-                className={mainStyles.input}
-                value={'Providencia'}
-              />
-            </div>
-            <div className={styles.wrapper}>
-              <h5 className={styles.title}>Link</h5>
-              <input type="text" className={mainStyles.link} value={'http'} />
-            </div>
-            <div className={styles.buttonContainer}>
-              <button className={`${styles.button} ${styles.remove}`}>
-                <FiTrash size={20} />
-              </button>
-            </div>
+          <div className={mainStyles.buttonContainer}>
+            <SaveButton
+              handlePress={() => handleUpdateResourceById(resourceId)}
+              isLoading={isLoading}
+            />
           </div>
-        </div>
-      </div>
-      <div className={mainStyles.buttonContainer}>
-        <SaveButton />
-      </div>
+          {dialogIsOpen && (
+            <DeleteResourceModal
+              dialogIsOpen={dialogIsOpen}
+              setDialogIsOpen={setDialogIsOpen}
+              message="¿Estás seguro que deseas eliminar este recurso?"
+              okButtontext="Eliminar"
+              cancelButtonText="Cancelar"
+              handleDelete={() => {
+                handleDeleteResourceById(resourceId)
+                handleGoBack()
+              }}
+              isLoading={isLoading}
+            />
+          )}
+        </>
+      )}
     </section>
   )
 }
