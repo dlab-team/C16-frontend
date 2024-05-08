864cbba9c39cee6833b11c279419a90b0df8be9e
Sebastian <sebas@Sebastians-MacBook-Air.local>
2024-05-07 20:26:59 -0500

    Add Partner Service Api

---
+++ src/services/api/api.partners.service.js
@@ -0,0 +1,105 @@
+const BASE_URL = 'https://c16-backend.onrender.com/api'
+
+// Trae todos los Socios de la API
+export const getAllPartners = async (page = '1', idToken) => {
+  const response = await fetch(`${BASE_URL}/partners/?page=${page}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${idToken}`,
+    },
+  })
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`)
+  }
+  return response.json()
+}
+
+// Trae los Socios por ID desde la API
+
+export const getPartnerById = async (partnerId, idToken) => {
+  const response = await fetch(`${BASE_URL}/partners/${partnerId}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${idToken}`,
+    },
+  })
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`)
+  }
+
+  return response.json()
+}
+
+//Elimina un Socio por ID de la API
+
+export const deletePartnerById = async (partnerId, idToken) => {
+  const response = await fetch(`${BASE_URL}/partners/${partnerId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${idToken}`,
+    },
+  })
+
+  return response
+}
+
+//Actualiza los Socios por ID desde la API
+
+export const updatePartnerById = async (partnerId, newData, idToken) => {
+  const url = `${BASE_URL}/partners/${partnerId}`;
+  const response = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      Authorization: `Bearer ${idToken}`,
+    },
+    body: newData,
+  });
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return response;
+};
+
+//Crear un Socio en la la API
+
+export const createPartner = async (data, idToken) => {
+  const response = await fetch(`${BASE_URL}/partners`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${idToken}`,
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+// Trae un Socio por palabra desde la API
+
+export const getPartnerByWord = async (search, page = '1', idToken) => {
+  const response = await fetch(
+    `${BASE_URL}/partners?search=${search}&page=${page}`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${idToken}`,
+      },
+    },
+  )
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`)
+  }
+  return response.json()
+}
